e42aa051c9defe20d8469c5dfeddf66331ee9202
zombotar <zombotar@gmail.com>
2016-11-06 16:57:45 +0300

    getUserID func added

--- WcfService1/Service1.svc.cs
+++ WcfService1/Service1.svc.cs
@@ -32,12 +32,41 @@ namespace WcfService1
             return composite;
         }
 
+        private int GetUserID(string _username, string _p_hash)
+        {
+            int id = -1;
+
+            SQLiteConnection connection =
+                new SQLiteConnection(string.Format("Data Source={0};", MyClass.Instance.currentDirectory + MyClass.Instance.databaseName));
+            string commandText = String.Format("select id from USERS where name = \"{0}\" and p_hash = \"{1}\" ;", _username, _p_hash);
+            SQLiteCommand command =
+                new SQLiteCommand(commandText, connection);
+
+            try
+            {
+                connection.Open();
+                var result = command.ExecuteReader();
+                if (result.Read())
+                    id = Int32.Parse(result["id"].ToString());
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return id;
+        }
+
         public AuthenticationResult Authentication(string _username, string _p_hash)
         {
             AuthenticationResult result = new AuthenticationResult();
             result.resultCode = -1;
             var x = MyClass.Instance;
-
+            result.userId = GetUserID(_username, _p_hash);
             return result;
         }
     }
@@ -95,12 +124,25 @@ namespace WcfService1
                 (
                 "CREATE TABLE IF NOT EXISTS \"USERS\" (\"id\" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , \"name\" TEXT NOT NULL , \"p_hash\" BLOB NOT NULL  UNIQUE ); " +
                 "CREATE TABLE IF NOT EXISTS \"GROUPS\" (\"id\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \"groupname\" TEXT NOT NULL); " +
-                "CREATE TABLE IF NOT EXISTS \"USER_TO_GROUP\" (\"userid\" INTEGER, \"groupid\" INTEGER, PRIMARY KEY (userid, groupid), FOREIGN KEY (userid) REFERENCES USERS(id), FOREIGN KEY (groupid) REFERENCES GROUPS(id) );"
+                "CREATE TABLE IF NOT EXISTS \"USER_TO_GROUP\" (\"userid\" INTEGER, \"groupid\" INTEGER, PRIMARY KEY (userid, groupid), FOREIGN KEY (userid) REFERENCES USERS(id), FOREIGN KEY (groupid) REFERENCES GROUPS(id) ); " +
+                "CREATE TABLE IF NOT EXISTS \"FILE_META\" (\"id\" INTEGER PRIMARY KEY  AUTOINCREMENT NOT NULL, \"filepath\" TEXT, \"filesize\" INTEGER, \"ownerid\" INTEGER, \"groupid\" INTEGER, \"access_bitset\" INTEGER NOT NULL, FOREIGN KEY (ownerid) REFERENCES USERS(id), FOREIGN KEY (groupid) REFERENCES GROUPS(id) );" +
+                "CREATE TABLE IF NOT EXISTS \"LOG_TABLE\" (\"id\" INTEGER PRIMARY KEY AUTOINCREMENT, \"userid\" INTEGER NOT NULL, \"groupid\" INTEGER, \"date_time\" TEXT, \"value\" TEXT, \"sql_value\" TEXT, FOREIGN KEY (userid) REFERENCES USERS(id) );"
                 );
             SQLiteCommand command =
                 new SQLiteCommand(commandText, connection);
-            connection.Open();
-            command.ExecuteNonQuery();
+
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            } catch (Exception e)
+            {
+                
+            }
+            finally
+            {
+                connection.Close();
+            }
 
             if (!Directory.Exists(currentDirectory + mainDirectoryName))
             {
